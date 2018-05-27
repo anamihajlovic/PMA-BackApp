d2bfd5cfc16e11dfd0261885a403a5029b16c0bd
Tamara <tamarica94mrksic@gmail.com>
2018-05-27 17:15:17 +0200

    Impelemented sending shows by facilitiy.

--- src/main/java/com/showbook/pma/controller/ShowController.java
+++ src/main/java/com/showbook/pma/controller/ShowController.java
@@ -1,9 +1,14 @@
 package com.showbook.pma.controller;
 
+import com.showbook.pma.model.Reservation;
+import com.showbook.pma.model.Show;
 import com.showbook.pma.service.ShowService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/shows")
@@ -15,5 +20,11 @@ public class ShowController {
     public ShowController(final ShowService showService) {
         this.showService = showService;
 
+    }
+
+    @CrossOrigin(origins="*")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<List<Show>> getShowsByFacilitiy(@PathVariable("id") Long FacilitiyId){
+        return new ResponseEntity<>(showService.findShowsByFacility(FacilitiyId), HttpStatus.OK);
     }
 }
--- src/main/java/com/showbook/pma/service/ShowService.java
+++ src/main/java/com/showbook/pma/service/ShowService.java
@@ -1,11 +1,15 @@
 package com.showbook.pma.service;
 
 
+import com.showbook.pma.model.Event;
+import com.showbook.pma.model.Repertoire;
 import com.showbook.pma.model.Show;
 import com.showbook.pma.repository.ShowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,19 +18,47 @@ public class ShowService {
     @Autowired
     private ShowRepository showRepository;
 
-    public Show findOne(Long id){
+    @Autowired
+    private FacilityService facilityService;
+
+    @Autowired
+    private RepertoireService repertoireService;
+
+    @Autowired
+    private EventService eventService;
+
+    public Show findOne(Long id) {
         return showRepository.findOne(id);
     }
 
-    public List<Show> findAll(){
+    public List<Show> findAll() {
         return showRepository.findAll();
     }
 
-    public Show save(Show show){
+    public Show save(Show show) {
         return showRepository.save(show);
     }
 
-    public void delete(Show show){
+    public void delete(Show show) {
         showRepository.delete(show);
+    }
+
+    public List<Show> findShowsByFacility(Long facilltyId) {
+        List<Show> result = new ArrayList<>();
+        List<Repertoire> allRepertoiresForFacility = facilityService.findOne(facilltyId).getRepertoires();
+        for (int i = 0; i< allRepertoiresForFacility.size();i++) {
+            System.out.println("Repertoar  id je " + allRepertoiresForFacility.get(i).getId());
+            if(allRepertoiresForFacility.get(i).getStart().after(new Date())){
+                System.out.println("Repertoar posle danas id je " + allRepertoiresForFacility.get(i).getId());
+                List<Event> foundEvents = allRepertoiresForFacility.get(i).getEvents();
+                for(int m =0; m< foundEvents.size();m++) {
+                    if(!result.contains(foundEvents.get(m).getShow())) {
+                        System.out.println("Show je dodat a id je " + foundEvents.get(m).getShow().getId());
+                        result.add(foundEvents.get(m).getShow());
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
